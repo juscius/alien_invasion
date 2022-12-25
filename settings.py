67818939cb81eacf8a81c8fdfe9a7c67e4f0674e
Justas <justas.vasiljevas@gmail.com>
2022-12-25 13:24:15 +0200

    Ship midbottom

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ class Settings:
 	"""A class to store all settings for Alien Invasion."""
 
 	def __init__(self):
-		self.screen_width = 1200
-		self.screen_height = 600
+		self.screen_width = 1300
+		self.screen_height = 700
 		self.background_color = (230, 230, 230)
