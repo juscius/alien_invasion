9486b926644bffc51e57420d7b0c580ba178f5b2
Justas <justas.vasiljevas@gmail.com>
2022-12-26 18:27:37 +0200

    Moving Fleet

--- alien.py
+++ alien.py
@@ -7,7 +7,7 @@ class Alien(Sprite):
 		super().__init__()
 
 		self.screen = ai_game.screen
-
+		self.settings = ai_game.settings
 		self.image = pygame.image.load('images/alien.bmp')
 		self.rect = self.image.get_rect()
 
@@ -18,3 +18,13 @@ class Alien(Sprite):
 		# Store the alien's exact horizontal position.
 		self.x = float(self.rect.x)
 
+	def update(self):
+		"""Move the alien left or right."""
+		self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+		self.rect.x = self.x
+
+	def check_edges(self):
+		"""Return True if alien is at edge of screen."""
+		screen_rect = self.screen.get_rect()
+		if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+			return True
